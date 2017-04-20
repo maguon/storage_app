5b4b8af7058865a3a075e4109693d8b8ab82327f
lingxue <info@myxxjs.com>
2017-04-20 13:51:25 +0800

    init app project

--- index.android.js
+++ index.android.js
@@ -11,22 +11,35 @@ import {
   Text,
   View
 } from 'react-native';
+import {Body,Container,Content,Header,Left,Right,Title} from 'native-base';
+import { Provider ,connect} from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import {Scene, Router,Actions,TabBar} from 'react-native-router-flux';
 
+
+import reducers from './src/reducers';
+
+
+const store = compose(
+    applyMiddleware(ReduxThunk)
+)(createStore)(reducers);
 export default class storage_app extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <Provider store={store}>
+          <Container>
+            <Content>
+              <Header >
+                <Left></Left>
+                <Body style={{backgroundColor:"transparent"}}>
+                <Title style={{paddingLeft:70}}>Setting</Title>
+                </Body>
+                <Right></Right>
+              </Header>
+            </Content>
+          </Container>
+        </Provider>
     );
   }
 }
