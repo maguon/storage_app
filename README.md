20c886b5ce33bb068bb7dfd427042d5224d603e0
lingxue <info@myxxjs.com>
2017-04-21 15:21:16 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# the project for logistic storage app
+
+
+## intall
+   npm install
+
+   react-native link
+
+## start
+
+
+## react-native run-android
--- index.android.js
+++ index.android.js
@@ -8,13 +8,11 @@ import React, { Component } from 'react';
 import {AppRegistry} from 'react-native';
 import Android_main from './src/Android_main';
 
-import {Container,Content} from 'native-base';
-
-export default class nt extends Component {
+/*export default class nt extends Component {
     render() {
         return (
             <Container><Content></Content></Container>
         );
     }
-}
-AppRegistry.registerComponent('storage_app', () => nt);
+}*/
+AppRegistry.registerComponent('storage_app', () => Android_main);
