272f0123bdd697ef540d3c81233a3e775855de98
rb yu <rbyu@rbdeMacBook-Air.local>
2017-05-22 15:45:40 +0800

    ok

--- src/reducers/LoginReducer.js
+++ src/reducers/LoginReducer.js
@@ -6,11 +6,16 @@ const initialState = {
     isLoginSuccess: false,
     isJump: false,
     user: {
-        userId: 38,
-        token: 'FvocqWoSwbzrv_4ScNX7amMUWC8=lwHMeYf_cd030af3c0146effcede4b0fbc37be14d0fce351eb6e8da42930c91ab9c09f799c7864b69158d22a754dd14e5f70f019s',
+        // userId: 38,
+        // token: 'FvocqWoSwbzrv_4ScNX7amMUWC8=lwHMeYf_cd030af3c0146effcede4b0fbc37be14d0fce351eb6e8da42930c91ab9c09f799c7864b69158d22a754dd14e5f70f019s',
+        // userType: 1,
+        // userStatus: 1,
+        // mobile: '18888'
+        userId: 0,
+        token: '',
         userType: 1,
         userStatus: 1,
-        mobile: '18888'
+        mobile: ''
     }
 }
 
